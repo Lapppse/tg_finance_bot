1b18cd916148cbd00db97da4fa26c2e0a1898118
Lapppse <81081725+Lapppse@users.noreply.github.com>
2024-09-03 17:22:12 +0300

    feat(db): add create db function and query

---
+++ bot/db/__init__.py
@@ -0,0 +1,29 @@
+__all__ = ("Conn", "create_db")
+
+import sqlite3
+
+from loguru import logger
+
+
+class Conn:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.file_path)
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None:
+            raise Exception(exc_type, exc_value, traceback)
+        self.cursor.close()
+        self.conn.commit()
+        self.conn.close()
+
+
+def create_db():
+    with Conn("bot/db/test.db") as db:
+        with open("bot/db/queries/create_expenses.sql", "r") as f:
+            db.execute(f.read())
+            logger.debug("created expenses table in db")
